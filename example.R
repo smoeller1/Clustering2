4d415bc98f58c1d94dbccde7392aae465cf0409f
smoeller1 <simon@quasily.com>
2016-02-16 11:32:29 -0600

    Added EM calc and plotting

--- example.R
+++ example.R
@@ -1,5 +1,14 @@
+###K-means
 data=read.csv("c:/Users/smoeller/Documents/MSCS/CS5542/Clustering 2/data.csv")
 x<-rbind(data$Lat,data$Lon)
 x<-t(x)
 km<-kmeans(x,13,20)
 plot(x,col=km$cluster)
+
+###EM
+library(EMCluster)
+data=read.csv("c:/Users/smoeller/Documents/MSCS/CS5542/Clustering 2/data.csv")
+x<-rbind(data$Lat,data$Lon)
+x<-t(x)
+ret.1<-starts.via.svd(x,nclass=13,method="em")
+plotem(ret.1,x)
